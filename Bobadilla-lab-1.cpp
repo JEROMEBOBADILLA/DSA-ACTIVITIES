0d92720c2864b5b5d1f6572736a435cc18556ef7
JEROMEBOBADILLA <24-13730@g.batstate-u.edu.ph>
2025-02-15 11:36:46 +0800

    GFT

--- Bobadilla-lab-1.cpp
+++ Bobadilla-lab-1.cpp
@@ -10,7 +10,7 @@ void traverseArray(){
     cout << "Showing elements in my array\n";
     for(int i= 0; i< maxElem; i++){
         cout<< to_string(myArray[i]) + " | ";
-    }
+    }z
 }
 
 string insertAtTheEnd(int num){
@@ -76,7 +76,6 @@ int main(){
  insertAtTheBeginning(19);
 
 traverseArray();
-
 
 
     
